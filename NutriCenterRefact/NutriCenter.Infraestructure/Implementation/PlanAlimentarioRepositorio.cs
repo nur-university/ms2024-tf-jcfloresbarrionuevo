8d21b37ab5a8e57e0d0f33beb2aa42b84a506c96
jcfloresbarrionuevo <juancarlosflores777@gmail.com>
2024-12-18 22:53:40 -0400

    segunda cargada de archivos
    completo la carga de archivos de la solucion

---
+++ NutriCenterRefact/NutriCenter.Infraestructure/Implementation/PlanAlimentarioRepositorio.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NutriCenter.Domain.EntitiesDomain;
+using NutriCenter.Infraestructure.DBContext;
+using NutriCenter.Infraestructure.Interfaces;
+
+namespace NutriCenter.Infraestructure.Implementation;
+
+public class PlanAlimentarioRepositorio:IPlanAlimentarioRepositorio
+{
+    private readonly AppDbContext _context;
+
+    public PlanAlimentarioRepositorio(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task AgregarPlanAsync(PlanAlimentario plan)
+    {
+        try
+        {
+            await _context.PlanAlimentario.AddAsync(plan);
+            await _context.SaveChangesAsync();
+        }
+        catch (Exception ex)
+        {
+
+            throw;
+        }
+    }
+
+    public async Task<List<PlanAlimentario>> ObtenerPlanAsync()
+    {
+        return await _context.PlanAlimentario.ToListAsync();
+    }
+}
---
+++ NutriCenterRefact/NutriCenter.Mapper/AutoMapperProfile.cs
@@ -0,0 +1,24 @@
+﻿using AutoMapper;
+using NutriCenter.Aplication.DTOs;
+using NutriCenter.Domain.EntitiesDomain;
+
+namespace NutriCenter.Mapper;
+
+public class AutoMapperProfile : Profile
+{
+    public AutoMapperProfile()
+    {
+        // Mapeo de Receta a RecetaDTO
+        CreateMap<Receta, RecetaDTO>()
+            .ForMember(dest => dest.Ingredientes, opt => opt.MapFrom(src => src.Ingredientes)).ReverseMap();
+
+        // Mapeo de IngredienteReceta a IngredientesDTO
+        CreateMap<IngredienteReceta, IngredientesDTO>().ReverseMap();
+
+        CreateMap<TiempoComidaDTO,TiempoComida>().ReverseMap();
+
+        CreateMap<PlanAlimentarioDTO, PlanAlimentario>().ReverseMap();
+
+        CreateMap<PlanRecetaTiempo, PlanRecetaTiempoDTO>().ReverseMap();            
+    }
+}
